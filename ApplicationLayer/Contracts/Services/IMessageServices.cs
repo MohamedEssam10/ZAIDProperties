355afc74e552999c986d98d0e652eaeb3a727f2d
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2025-07-12 01:39:56 +0300

    MessageSpecParams

--- ApplicationLayer/Contracts/Services/IMessageServices.cs
+++ ApplicationLayer/Contracts/Services/IMessageServices.cs
@@ -11,7 +11,7 @@ namespace ApplicationLayer.Contracts.Services
 {
     public interface IMessageServices
     {
-        Task<APIResponse<Pagination<MessageDTOResponse>>> GetAll(SpecParams Params);
+        Task<APIResponse<Pagination<MessageDTOResponse>>> GetAll(MessageSpecParams Params);
         Task<APIResponse<MessageDTOResponse>> GetById(int id);
         Task<APIResponse<string>> Add(MessageDTORequest message);
         Task<APIResponse<string>> Delete(int id);
